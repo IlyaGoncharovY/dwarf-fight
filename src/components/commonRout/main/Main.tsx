b33816ef50cd3584193efa323cb7c6bee58c1a24
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-01-30 20:12:17 +0300

    feat(animate route) added AnimatedRoutes.tsx component for render animate routing in app

---
+++ src/components/commonRout/main/Main.tsx
@@ -0,0 +1,11 @@
+import {BrowserRouter} from 'react-router-dom';
+
+import {AnimatedRoutes} from '../animatedRoutes/AnimatedRoutes.tsx';
+
+export const Main = () => {
+  return (
+    <BrowserRouter>
+      <AnimatedRoutes />
+    </BrowserRouter>
+  );
+};
