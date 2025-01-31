7c8b0e6d33ab6023e56b2d3f8e577cf593cfba0d
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-01-31 23:57:50 +0300

    feat(data for buttons) added arenaData.ts for render buttons in block action

---
+++ src/common/dataSet/arenaData/arenaData.ts
@@ -0,0 +1,28 @@
+import {DirectionType} from '../../../pages/arena/reducer/fightSlice.ts';
+import leftArrow from '../../../assets/images/left.svg';
+import centerArrow from '../../../assets/images/center.svg';
+import rightArrow from '../../../assets/images/right.svg';
+
+export type buttonsForActivityInArenaType = {
+  id: number
+  value: DirectionType
+  url: string
+}
+
+export const buttonsForActivityInArena: buttonsForActivityInArenaType[] = [
+  {
+    id: 1,
+    value: 'left',
+    url: leftArrow,
+  },
+  {
+    id: 2,
+    value: 'center',
+    url: centerArrow,
+  },
+  {
+    id: 3,
+    value: 'right',
+    url: rightArrow,
+  },
+];
