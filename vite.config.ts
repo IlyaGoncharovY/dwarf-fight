9d8a2a1fc704a5865d7f6089aea224b9c99f8f2d
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-02-02 22:14:39 +0300

    fix(ci) added modules and exclude

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   base: '/dwarf-fight',
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
   server: {
     watch: {
       usePolling: true,
