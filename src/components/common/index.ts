50f66606ef224bea81d28c329e781274a52ed299
IlyaGoncharovY <goncharov.i@rambler.ru>
2025-03-11 14:50:00 +0300

    feat(BattleLog) change directory to common component. added custom hook useMessageForBattleLog.ts for BattleLog.

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@ export * from './unitIcon/UnitIcon.tsx';
 export * from './wrapper/PageWrapper.tsx';
 export * from './button/UIButton.tsx';
 export * from './loader/Loader.tsx';
+export * from './battleLog/BattleLog.tsx';
